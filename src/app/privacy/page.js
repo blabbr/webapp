7113f4d96cf3c0afb7fba8011d0f4ad44373912e
blabbr.buzz <141965847+blabbr@users.noreply.github.com>
2023-08-11 15:50:58 -0400

    fixed typo

--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -8,7 +8,7 @@ export default function Page() {
         <p>We take absolutely no responsibility for your privacy.</p>
         <p>Our budget for your privacy and taking care of your data is $0.</p>
         <p>If you fear for your privacy and data, please go use a platform you trust more, but we do not recommend Facebook, Instagram, TikTok etc.. - we have heard they have done some bad stuff with their user data in the past - but we cannot be sure, this is not an accusation, more of a musing.</p>
-        <p>Anyway, noi privacy here - use the website if you want, else -have a nice day!</p>
+        <p>Anyway, no privacy here - use the website if you want, else -have a nice day!</p>
     </div>
     )
 }
